04019e53be84f3cfcc91d42e420e0dc376c2e027
tumiemosweu <imosweuk@gmail.com>
2020-06-01 09:33:07 +0200

    fix death report model get consent version

--- potlako_prn/models/death_report.py
+++ potlako_prn/models/death_report.py
@@ -127,30 +127,21 @@ class DeathReport(SiteModelMixin, ActionModelMixin, BaseUuidModel):
         null=True)
 
     def get_consent_version(self):
-        subject_screening_cls = django_apps.get_model(
-            'td_maternal.subjectscreening')
-        consent_version_cls = django_apps.get_model(
-            'td_maternal.tdconsentversion')
+        subject_consent_cls = django_apps.get_model(
+            'td_maternal.subject_consent')
         try:
-            subject_screening_obj = subject_screening_cls.objects.get(
+            subject_consent_obj = subject_consent_cls.objects.get(
                 subject_identifier=self.subject_identifier)
-        except subject_screening_cls.DoesNotExist:
+        except subject_consent_cls.DoesNotExist:
             raise ValidationError(
-                'Missing Subject Screening form. Please complete '
+                'Missing Subject Consent form. Please complete '
                 'it before proceeding.')
         else:
-            try:
-                consent_version_obj = consent_version_cls.objects.get(
-                    screening_identifier=subject_screening_obj.screening_identifier)
-            except consent_version_cls.DoesNotExist:
-                raise ValidationError(
-                    'Missing Consent Version form. Please complete '
-                    'it before proceeding.')
-            return consent_version_obj.version
+            return subject_consent_obj.version
 
     def save(self, *args, **kwargs):
         self.consent_version = self.get_consent_version()
-        super(DeathReportModelMixin, self).save(*args, **kwargs)
+        super(DeathReport, self).save(*args, **kwargs)
 
     def natural_key(self):
         return (self.subject_identifier,)
