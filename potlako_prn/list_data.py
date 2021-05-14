156b6469fa213c39c953b211a614b4c57de89938
tumiemosweu <imosweuk@gmail.com>
2021-05-14 12:19:02 +0200

    update coordinator exit options

--- potlako_prn/list_data.py
+++ potlako_prn/list_data.py
@@ -3,19 +3,17 @@ from edc_list_data import PreloadData
 
 list_data = {
     'potlako_prn.componentsreceived': [
-        ('provider_edication', 'Provider education'),
-        ('diagnostic_facilitation', 'Diagnostic facilitation (pre-biopsy/test)'),
-        ('access_to_diagnostic_results',
-         'Access to diagnostic results (e.g. histology)'),
-        ('cancer_treatment_facilitation_post_test_results',
-         'Cancer treatment facilitation post-test results'),
-        ('retention_or_completion_of_cancer_treatment',
-         'Retention or completion of cancer treatment'),
+        ('potlako+_iec_material', 'Potlako+ IEC material'),
+        ('public_campaign',
+         'Public campaign'),
+        ('community_leader_info',
+         'information from community leader'),
+        ('group_teaching',
+         'Group teaching'),
         (NONE, 'None'),
         (OTHER, 'Other (specify)'),
     ],
 }
-
 
 preload_data = PreloadData(
     list_data=list_data)
