6e0b0ee37f00218c24804dc5fa319d5a3fd5b1b7
nmunatsibw <mcturnergarai@gmail.com>
2023-04-25 10:05:23 +0200

    :ambulance: fix(Action Items): trigger death report on off study action

--- potlako_prn/models/__init__.py
+++ potlako_prn/models/__init__.py
@@ -1,4 +1,3 @@
 from .coordinator_exit import CoordinatorExit
 from .death_report import DeathReport
-from .signals import subject_offstudy_on_post_save, death_report_on_post_save
 from .subject_offstudy import SubjectOffStudy
